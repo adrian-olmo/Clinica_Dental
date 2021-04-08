24ca720fb388119d4a4af8948b22a9cf252f9add
Adrian Olmo <47026705+adrian-olmo@users.noreply.github.com>
2021-04-08 18:08:41 +0200

    Modelo usuario relaciones

--- models/user.js
+++ models/user.js
@@ -1,24 +1,31 @@
 'use strict';
-import pkg from 'sequelize';
-import database  from "../database.js";
+import pkg from "sequelize";
+import database from "../database.js";
+import dating from "./dating.js";
 
 const { Model, DataTypes } = pkg;
 
 export class User extends Model {
-  static associate(models) {
-    // define association here
-  }
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+        // define association here
+    }
 };
-
 User.init({
-  name: DataTypes.STRING,
-  lastname: DataTypes.STRING,
-  phone: DataTypes.INTEGER,
-  history: DataTypes.INTEGER,
-  role: DataTypes.INTEGER,
-  createdAt: DataTypes.DATE,
-  updatedAt: DataTypes.DATE
+    name: DataTypes.STRING,
+    lastname: DataTypes.STRING,
+    phone: DataTypes.STRING,
+    history: DataTypes.STRING,
+    role: DataTypes.STRING,
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE
 }, {
-  modelName: "User",
-  sequelize: database,
+    sequelize: database,
+    modelName: 'User',
 });
+
+User.hasMany(dating, { foreignKey: 'userID' });
