e5e7e0f35761d82a7a6b1879d0dfe2d6bbca6f2f
Adrian Olmo <47026705+adrian-olmo@users.noreply.github.com>
2021-04-10 18:34:42 +0200

    Intento de Autentificacion

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 import express from 'express';
+
+//Routes
 import datingRoutes from './routes/datings.routes.js';
 import userRoutes from "./routes/users.routes.js";
+import doctorRoutes from "./routes/doctors.routes.js";
+import authRoutes from './routes/auth.routes.js';
+
+//MiddleWare
+import checkJwt from "./middleware/checkJwt.js";
 
 
 const app = express();
 app.use(express.json());
 
+app.use('/auth', authRoutes)
 app.use('/users', userRoutes);
 app.use('/datings', datingRoutes);
+app.use('/doctors', checkJwt, doctorRoutes);
 
 app.listen(3000, () => {
     console.log("servidor conectado en el puerto 3000")
