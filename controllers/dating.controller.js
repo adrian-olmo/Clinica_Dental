1c9d3fa0a1386a90f6d6b81e18c4cc0f4aabe5a6
Adrian Olmo <47026705+adrian-olmo@users.noreply.github.com>
2021-04-11 12:55:40 +0200

    Controllador Dating

--- controllers/dating.controller.js
+++ controllers/dating.controller.js
@@ -1,6 +1,6 @@
 import database from "../database.js";
 import { Dating } from "../models/dating.js";
-import { Dating, User, Doctors } from "./models/index.js";
+
 
 export const datingController = {
     list: async(req, res) => {
