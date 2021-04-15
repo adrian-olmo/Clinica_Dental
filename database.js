1b4441d65f7b35d8203a71252614b34c344ac613
Adrian Olmo <47026705+adrian-olmo@users.noreply.github.com>
2021-04-15 10:54:14 +0200

    Clinica dental endpoints terminados

--- database.js
+++ database.js
@@ -1,28 +1,31 @@
-import sequelize from 'sequelize'
+import sequelize from "sequelize";
 
 const db = "db_clinica";
-const password = "root1234";
+const password = "";
 const username = "root";
 
 const database = new sequelize(db, username, password, {
-    dialect: "mysql",
-    port: 3306,
-    define: {
-        timestamps: false
-    }
+  dialect: "mysql",
+  port: 3306,
+  define: {
+    timestamps: false,
+  },
 });
 
-database.authenticate().then(() => {
-    async() => {
-        console.log("database connected")
-        try {
-            await database.sync({ force: true })
-        } catch (error) {
-            console.log(error.message)
-        }
-    }
-}).catch((e) => {
-    console.log(e.message)
-});
+database
+  .authenticate()
+  .then(() => {
+    async () => {
+      console.log("database connected");
+      try {
+        await database.sync({ force: true });
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+  })
+  .catch(e => {
+    console.log(e.message);
+  });
 
 export default database;
